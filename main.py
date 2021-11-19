7c5a9c5ddd972cd31a734b7816b1e77bf6e63ca0
DrgnFireYellow <86269995+DrgnFireYellow@users.noreply.github.com>
2021-11-19 12:20:13 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+class Game:
+    def __init__(self, name):
+        name = self.name
+        window = tk.Tk()
+        window.title(name)
+
+class Player:
+    def __init__(self):
+        
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='simplegame',
+    version='1.0.0beta',
+    description='Making games in python has never been simpler! (Please Note that you will need to have Tk installed!)',
+    author='DrgnFireYellow',
+    maintainer='DrgnFireYellow',
+    url='http://github.com/DrgnFireYellow/simplegame'
+)
